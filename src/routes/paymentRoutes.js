440d77c2ae592e35d8dfbb77daf89d962e20550a
Cristian <crisemadiaz@gmail.com>
2025-04-29 23:33:30 -0300

    Se corrige paymentRoutes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
-const { route } = require('../../../order-service/src/routes/orderRoutes');
 
 //crear pago
 router.post('/payments', paymentController.createPayment);
